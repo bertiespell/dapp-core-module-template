02c41956367882aa789bccd7165ada2a83127844
Bertie <berniesnell@gmail.com>
2020-01-10 15:47:30 +0000

    move getActiveDelegates, updateVoteBalances and buildDelegateRanking into plugin - inject database service

--- src/manager.ts
+++ src/manager.ts
@@ -1,10 +1,21 @@
 import { app } from "@arkecosystem/core-container";
-import { Logger } from "@arkecosystem/core-interfaces";
+import { Database, Logger, Shared, State } from "@arkecosystem/core-interfaces";
+import { IDatabaseService } from "@arkecosystem/core-interfaces/dist/core-database";
+import { Wallets } from "@arkecosystem/core-state";
+import { roundCalculator } from "@arkecosystem/core-utils";
+import { Crypto, Enums, Identities, Interfaces, Utils } from "@arkecosystem/crypto";
+import cloneDeep from "lodash.clonedeep";
+
+// export interface IDappManagerOptions {
+//     database_service: IDatabaseService;
+// };
 
 export class DappManager {
     private readonly logger: Logger.ILogger = app.resolvePlugin<Logger.ILogger>(
         "logger"
     );
+
+    private databaseService: IDatabaseService = undefined;
 
     /**
      * Your dApp init code goes here
@@ -12,6 +23,11 @@ export class DappManager {
      */
     public start(options: any) {
         this.logger.info("Initialization of dApp");
+        if (options.database_service) {
+            this.databaseService = options.database_service;
+        } else {
+            // error handling
+        }
     }
 
     /**
@@ -21,8 +37,74 @@ export class DappManager {
         this.logger.info("Stopping dApp");
     }
 
+    public async getActiveDelegates(
+        roundInfo?: Shared.IRoundInfo,
+        delegates?: State.IWallet[],
+        forgingDelegates?: State.IWallet[],
+    ): Promise<State.IWallet[]> {
+ 
+        if (!roundInfo) {
+            const database: Database.IDatabaseService = app.resolvePlugin("database");
+            const lastBlock = await database.getLastBlock();
+            roundInfo = roundCalculator.calculateRound(lastBlock.data.height);
+        }
+
+        const { round } = roundInfo; // check the round
+
+        if (
+            forgingDelegates &&
+            forgingDelegates.length &&
+            forgingDelegates[0].getAttribute<number>("delegate.round") === round
+        ) {
+            return forgingDelegates;
+        }
+
+        // When called during applyRound we already know the delegates, so we don't have to query the database.
+        if (!delegates || delegates.length === 0) {
+            delegates = (await this.databaseService.connection.roundsRepository.findById(round)).map(({ round, publicKey, balance }) =>
+                Object.assign(new Wallets.Wallet(Identities.Address.fromPublicKey(publicKey)), {
+                    publicKey,
+                    attributes: {
+                        delegate: {
+                            voteBalance: Utils.BigNumber.make(balance),
+                            username: this.databaseService.walletManager.findByPublicKey(publicKey).getAttribute("delegate.username"),
+                        },
+                    },
+                }),
+            );
+        }
+
+        for (const delegate of delegates) {
+            delegate.setAttribute("delegate.round", round);
+        }
+
+        const seedSource: string = round.toString();
+        let currentSeed: Buffer = Crypto.HashAlgorithms.sha256(seedSource);
+
+        delegates = cloneDeep(delegates);
+        for (let i = 0, delCount = delegates.length; i < delCount; i++) {
+            for (let x = 0; x < 4 && i < delCount; i++ , x++) {
+                const newIndex = currentSeed[x] % delCount;
+                const b = delegates[newIndex];
+                delegates[newIndex] = delegates[i];
+                delegates[i] = b;
+            }
+            currentSeed = Crypto.HashAlgorithms.sha256(currentSeed);
+        }
+
+        return delegates;
+    }
+
     /**
-     * 
+     * Updates the vote balances of the respective delegates of sender and recipient.
+     * If the transaction is not a vote...
+     *    1. fee + amount is removed from the sender's delegate vote balance
+     *    2. amount is added to the recipient's delegate vote balance
+     *
+     * in case of a vote...
+     *    1. the full sender balance is added to the sender's delegate vote balance
+     *
+     * If revert is set to true, the operations are reversed (plus -> minus, minus -> plus).
      * @param sender 
      * @param recipient 
      * @param transaction 
@@ -30,7 +112,7 @@ export class DappManager {
      * @param lockTransaction 
      * @param revert 
      */
-    private updateVoteBalances(
+    public updateVoteBalances(
         sender: State.IWallet,
         recipient: State.IWallet,
         transaction: Interfaces.ITransactionData,
@@ -42,15 +124,14 @@ export class DappManager {
             transaction.type === Enums.TransactionType.Vote &&
             transaction.typeGroup === Enums.TransactionTypeGroup.Core
         ) {
-            // get vote information - pass to 
-            // consensus.updateVotes()
+
             const vote: string = transaction.asset.votes[0];
-            const delegate: State.IWallet = this.findByPublicKey(vote.substr(1));
+            const delegate: State.IWallet = this.databaseService.walletManager.findByPublicKey(vote.substr(1));
             let voteBalance: Utils.BigNumber = delegate.getAttribute("delegate.voteBalance", Utils.BigNumber.ZERO);
 
             if (vote.startsWith("+")) {
                 voteBalance = revert
-                    ? voteBalance.minus(sender.balance.minus(transaction.fee)) // how/where is this sender balance actually set?
+                    ? voteBalance.minus(sender.balance.minus(transaction.fee))
                     : voteBalance.plus(sender.balance);
             } else {
                 voteBalance = revert
@@ -62,11 +143,11 @@ export class DappManager {
         } else {
             // Update vote balance of the sender's delegate
             if (sender.hasVoted()) {
-                const delegate: State.IWallet = this.findByPublicKey(sender.getAttribute("vote"));
+                const delegate: State.IWallet = this.databaseService.walletManager.findByPublicKey(sender.getAttribute("vote"));
                 const amount =
                     transaction.type === Enums.TransactionType.MultiPayment &&
                     transaction.typeGroup === Enums.TransactionTypeGroup.Core
-                        ? transaction.asset.payments.reduce( // reduce over all different payments
+                        ? transaction.asset.payments.reduce(
                               (prev, curr) => prev.plus(curr.amount),
                               Utils.BigNumber.ZERO,
                           )
@@ -106,7 +187,7 @@ export class DappManager {
                 lockWallet.hasAttribute("vote")
             ) {
                 // HTLC Claim transfers the locked amount to the lock recipient's (= claim sender) delegate vote balance
-                const lockWalletDelegate: State.IWallet = this.findByPublicKey(lockWallet.getAttribute("vote"));
+                const lockWalletDelegate: State.IWallet = this.databaseService.walletManager.findByPublicKey(lockWallet.getAttribute("vote"));
                 const lockWalletDelegateVoteBalance: Utils.BigNumber = lockWalletDelegate.getAttribute(
                     "delegate.voteBalance",
                     Utils.BigNumber.ZERO,
@@ -125,10 +206,10 @@ export class DappManager {
             ) {
                 // go through all payments and update recipients delegates vote balance
                 for (const { recipientId, amount } of transaction.asset.payments) {
-                    const recipientWallet: State.IWallet = this.findByAddress(recipientId);
+                    const recipientWallet: State.IWallet = this.databaseService.walletManager.findByAddress(recipientId);
                     const vote = recipientWallet.getAttribute("vote");
                     if (vote) {
-                        const delegate: State.IWallet = this.findByPublicKey(vote);
+                        const delegate: State.IWallet = this.databaseService.walletManager.findByPublicKey(vote);
                         const voteBalance: Utils.BigNumber = delegate.getAttribute(
                             "delegate.voteBalance",
                             Utils.BigNumber.ZERO,
@@ -148,7 +229,7 @@ export class DappManager {
                 (transaction.type !== Enums.TransactionType.HtlcLock ||
                     transaction.typeGroup !== Enums.TransactionTypeGroup.Core)
             ) {
-                const delegate: State.IWallet = this.findByPublicKey(recipient.getAttribute("vote"));
+                const delegate: State.IWallet = this.databaseService.walletManager.findByPublicKey(recipient.getAttribute("vote"));
                 const voteBalance: Utils.BigNumber = delegate.getAttribute(
                     "delegate.voteBalance",
                     Utils.BigNumber.ZERO,
@@ -160,5 +241,54 @@ export class DappManager {
                 );
             }
         }
+    }
+
+    public buildDelegateRanking(roundInfo?: Shared.IRoundInfo): State.IWallet[] {
+        const delegatesActive: State.IWallet[] = [];
+
+        for (const delegate of this.databaseService.walletManager.allByUsername()) {
+            if (delegate.hasAttribute("delegate.resigned")) {
+                delegate.forgetAttribute("delegate.rank");
+            } else {
+                delegatesActive.push(delegate);
+            }
+        }
+
+        let delegatesSorted = delegatesActive
+            .sort((a, b) => {
+                const voteBalanceA: Utils.BigNumber = a.getAttribute("delegate.voteBalance");
+                const voteBalanceB: Utils.BigNumber = b.getAttribute("delegate.voteBalance");
+
+                const diff = voteBalanceB.comparedTo(voteBalanceA);
+                if (diff === 0) {
+                    if (a.publicKey === b.publicKey) {
+                        throw new Error(
+                            `The balance and public key of both delegates are identical! Delegate "${a.getAttribute(
+                                "delegate.username",
+                            )}" appears twice in the list.`,
+                        );
+                    }
+
+                    return a.publicKey.localeCompare(b.publicKey, "en");
+                }
+
+                return diff;
+            })
+            .map(
+                (delegate, i): State.IWallet => {
+                    const rank = i + 1;
+                    delegate.setAttribute("delegate.rank", rank);
+                    return delegate;
+                },
+            );
+
+        if (roundInfo) {
+            delegatesSorted = delegatesSorted.slice(0, roundInfo.maxDelegates);
+            for (const delegate of delegatesSorted) {
+                delegate.setAttribute("delegate.round", roundInfo.round);
+            }
+        }
+
+        return delegatesSorted;
     }
 }
